1d80a972c54efd4760e0fd7db82f588c1ce975ef
yang hyunil <fireyang@gmail.com>
2024-05-22 10:45:28 +0900

    최초 프로젝트셋팅
    여러분은 이프로젝트를 받아서
    제가 링크시키고 동작시킨것처럼
    만들어서 푸쉬하세요.

---
+++ WeWeb/src/main/java/edu/mit/c301/WebController.java
@@ -0,0 +1,12 @@
+package edu.mit.c301;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+	 @GetMapping("/yhi")
+	 public void yhi() {
+		 
+	 }
+}
